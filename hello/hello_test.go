1358b4f1fee005f6b077b0e07ee2942c92e7901f
cuijxin <cuijxin@126.com>
2020-04-10 05:51:17 +0000

    actions test

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestGreet(t *testing.T) {
 	result := Greet()
 	if result != "Hello GitHub Actions" {
-		t.Errorf("Greet() = %s; Expected Hello GitHub actions", result)
+		t.Errorf("Greet() = %s; Hello GitHub Actions", result)
 	}
 }
